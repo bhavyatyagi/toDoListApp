7c534330a76a496e715e037d8815669ba9696157
Bhavya Tyagi <bhavyatyagi16@gmail.com>
2021-09-21 12:46:31 +0530

    Git problems solved

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ const express = require('express');
 var bodyParser = require('body-parser');
 // databse config 
 const db = require('./config/mongoose');
-<<<<<<< HEAD
-const ToDoList = require('./models/toDoList');
-var jsdom = require('jsdom');
-$ = require('jquery')(new jsdom.JSDOM().window);
-const app = express();
-
-// Port 
-const port = 786;
-
-//inbuilt node module doesnt require installation
-const path = require('path');
-const { request } = require('http');
-const { response } = require('express');
-const { isArray } = require('jquery');
-=======
 const DB = 'mongodb+srv://bhavya2654:Bhavya@161201@cluster0.pffrg.mongodb.net/toDoList?retryWrites=true&w=majority';
 const ToDoList = require('./models/toDoList');
 const app = express();
@@ -37,7 +22,6 @@ const Contact = require('../contactListProject/models/contact');
 const {
     isArray
 } = require('jquery');
->>>>>>> 42d510c0ae5227488062082b2d824e6e97898b12
 
 
 // Setting up views and view engine 
@@ -71,14 +55,11 @@ app.get('/', function (request, response) {
 
 // creating list cards 
 app.post('/create-card', function (request, response) {
-<<<<<<< HEAD
-=======
 
     // If no deadlinesare given 
     if (request.body.date == "")
         request.body.date = "No Deadlines";
 
->>>>>>> 42d510c0ae5227488062082b2d824e6e97898b12
     ToDoList.create({
         description: request.body.description,
         category: request.body.category,
@@ -93,19 +74,13 @@ app.post('/create-card', function (request, response) {
     });
 });
 
-<<<<<<< HEAD
-=======
 // to check if the response incming is a list or just a single element 
->>>>>>> 42d510c0ae5227488062082b2d824e6e97898b12
 isArrayy = function (a) {
     return (!!a) && (a.constructor === Array);
 };
 app.get('/delete-card/', function (request, response) {
 
-<<<<<<< HEAD
-=======
     // if only single item to be deleted 
->>>>>>> 42d510c0ae5227488062082b2d824e6e97898b12
     let itemsToBeDeleted = new Array();
     if (isArrayy(request.query.check) == false) {
         ToDoList.findByIdAndDelete(request.query.check, function (error) {
@@ -116,10 +91,7 @@ app.get('/delete-card/', function (request, response) {
             return response.redirect('back');
         });
     }
-<<<<<<< HEAD
-=======
     // multiple items to be deleted 
->>>>>>> 42d510c0ae5227488062082b2d824e6e97898b12
     else {
         itemsToBeDeleted = request.query.check;
         for (let i = 0; i < itemsToBeDeleted.length; i++) {
